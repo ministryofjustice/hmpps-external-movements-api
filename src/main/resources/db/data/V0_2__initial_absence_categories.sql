29589857e72c9c4ab398bc04bb7559ce395badf5
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-09-08 17:30:11 +0100

    MIA-679 update reference data tests (#8)
    * MIA-679 update reference data tests

--- src/main/resources/db/data/V0_2__initial_absence_categories.sql
+++ src/main/resources/db/data/V0_2__initial_absence_categories.sql
@@ -78,7 +78,7 @@ on conflict do nothing;
 with categories as (
     (select *
      from (values ('FB', 'Accommodation-related', 1110, true),
-                  ('ET', 'Education or training ', 1120, true),
+                  ('ET', 'Education or training', 1120, true),
                   ('R3', 'Maintaining family ties', 1130, true),
                   ('PW', 'Paid work', 1140, true),
                   ('PAP', 'Prisoner apprenticeships pathway', 1150, true),
@@ -107,7 +107,7 @@ on conflict do nothing;
 with reasons as (
     (select *
      from (values ('FB', 'Accommodation-related', 1110, true),
-                  ('ET', 'Education or training ', 1120, true),
+                  ('ET', 'Education or training', 1120, true),
                   ('R3', 'Maintaining family ties', 1130, true),
                   ('PAP', 'Prisoner apprenticeships pathway', 1140, true),
                   ('YOTR', 'Other temporary release linked to sentence or resettlement plan', 1150, true),
@@ -132,7 +132,7 @@ with reasons as (
                   ('C6', 'Inpatient medical or dental appointment', 1340, true),
                   ('C5', 'Outpatient medical or dental appointment', 1350, true),
                   ('C7', 'Visit a dying relative', 1360, true),
-                  ('C4', 'Wedding or civil ceremony ', 1370, true),
+                  ('C4', 'Wedding or civil ceremony', 1370, true),
                   ('4', 'Other compassionate reason', 1380, true),
                   ('SE', 'Other security escort', 1390, true),
                   ('YRDR', 'RDR (Resettlement Day Release)', 1400, true),
@@ -145,60 +145,61 @@ with reasons as (
                   ('C9', 'Attend Religious Service', 1470, true),
                   ('TRNTAP', 'Transfer Via Temporary Release', 1480, true),
                   ('F4', 'Unescorted Court Appearance', 1490, true),
-                  ('10', 'Adventure and social welfare', 9400, false),
-                  ('FC', 'Adventure training', 9410, false),
-                  ('8', 'Community service volunteer', 9420, false),
-                  ('R7', 'Community service volunteer', 9430, false),
-                  ('FR', 'Deport of foreign national under early removal scheme', 9440, false),
-                  ('FE', 'Driving lessons', 9450, false),
+
+                  ('10', 'Adventure And Social Welfare', 9400, false),
+                  ('FC', 'Adventure Training', 9410, false),
+                  ('8', 'Community Service Volunteer', 9420, false),
+                  ('R7', 'Community Service Volunteer', 9430, false),
+                  ('FR', 'Deport of F/N under Early Removal Scheme', 9440, false),
+                  ('FE', 'Driving Lessons', 9450, false),
                   ('F2', 'Education', 9460, false),
-                  ('ETR3', 'Emergency temporary release - end of sentence', 9470, false),
-                  ('ETR1', 'Emergency temporary release - Pregnant/MBU', 9480, false),
-                  ('ETR2', 'Emergency temporary release - vulnerable', 9490, false),
-                  ('13', 'Employment interview', 9500, false),
-                  ('EL', 'ECL (End of Custody Licence)', 9510, false),
-                  ('ETL23', 'Extended temporary release 2023', 9520, false),
-                  ('ETR23', 'Extended temporary release 2023', 9530, false),
-                  ('7', 'External education classes', 9540, false),
-                  ('6', 'External sporting activity', 9550, false),
-                  ('FD', 'External team sports', 9560, false),
+                  ('ETR3', 'Emergency Temp Release - End of Sentence', 9470, false),
+                  ('ETR1', 'Emergency Temporary Release - Preg/MBU', 9480, false),
+                  ('ETR2', 'Emergency Temporary Release - Vulnerable', 9490, false),
+                  ('13', 'Employment Interview', 9500, false),
+                  ('EL', 'End of Custody Licence (ECL)', 9510, false),
+                  ('ETL23', 'Extended Temporary Release 2023', 9520, false),
+                  ('ETR23', 'Extended Temporary Release 2023', 9530, false),
+                  ('7', 'External Education Classes', 9540, false),
+                  ('6', 'External Sporting Activity', 9550, false),
+                  ('FD', 'External Team Sports', 9560, false),
                   ('2', 'Funeral', 9570, false),
-                  ('SP', 'Helping police with enquiries', 9580, false),
+                  ('SP', 'Helping Police With Enquiries', 9580, false),
                   ('IM', 'Immigration Hearing', 9590, false),
-                  ('ST', 'Interprison transfer', 9600, false),
-                  ('11', 'Interview for half-way house', 9610, false),
-                  ('F9', 'Interview for hostel accommodation', 9620, false),
-                  ('R4', 'Interview for hostel accommodation', 9630, false),
-                  ('R1', 'Job interview', 9640, false),
-                  ('F8', 'Job interview (under 12m)', 9650, false),
-                  ('I1', 'Local visit (incentive scheme)', 9660, false),
-                  ('14', 'Long home leave', 9670, false),
+                  ('ST', 'Interprison Transfer', 9600, false),
+                  ('11', 'Interview For Half Way House', 9610, false),
+                  ('F9', 'Interview For Hostel Accommodation', 9620, false),
+                  ('R4', 'Interview for Hostel Accommodation', 9630, false),
+                  ('R1', 'Job Interview', 9640, false),
+                  ('F8', 'Job Interview (Under 12m)', 9650, false),
+                  ('I1', 'Local Visit (Incentive Scheme)', 9660, false),
+                  ('14', 'Long Home Leave', 9670, false),
                   ('18', 'Marriage', 9680, false),
-                  ('FF', 'Opening bank accounts', 9690, false),
-                  ('21', 'Other (i.e. IPV, ID parades etc)', 9700, false),
-                  ('C8', 'Other special purpose', 9710, false),
-                  ('5', 'Outside hospital attendance', 9720, false),
-                  ('R9', 'Overnight resettlement unit/pres hostel', 9730, false),
-                  ('R2', 'Paid work placements', 9740, false),
-                  ('PWP', 'Paid work placements (youth)', 9750, false),
-                  ('17', 'Pre-parole leave', 9760, false),
-                  ('R6', 'Pre-parole release', 9770, false),
-                  ('C1', 'Primary carer', 9780, false),
-                  ('R8', 'Probation service workshop', 9790, false),
-                  ('9', 'Probation service workshops', 9800, false),
+                  ('FF', 'Opening Bank Accounts', 9690, false),
+                  ('21', 'Other (i.e IPV, ID Parades etc)', 9700, false),
+                  ('C8', 'Other Special Purpose', 9710, false),
+                  ('5', 'Outside Hospital Attendance', 9720, false),
+                  ('R9', 'Overnight Resettlement Unit/Pres Hostel', 9730, false),
+                  ('R2', 'Paid Work Placements', 9740, false),
+                  ('PWP', 'Paid Work Placements (Youth)', 9750, false),
+                  ('17', 'Pre Parole Leave', 9760, false),
+                  ('R6', 'Pre-Parole Release', 9770, false),
+                  ('C1', 'Primary Carer', 9780, false),
+                  ('R8', 'Probation Service Workshop', 9790, false),
+                  ('9', 'Probation Service Workshops', 9800, false),
                   ('TAP', 'ROTL', 9810, false),
-                  ('F3', 'Reparation projects', 9820, false),
-                  ('R5', 'Seeking other accommodation', 9830, false),
-                  ('FA', 'Seeking other accommodation (under 12m)', 9840, false),
-                  ('16', 'Short home leave', 9850, false),
-                  ('15', 'Terminal home leave', 9860, false),
+                  ('F3', 'Reparation Projects', 9820, false),
+                  ('R5', 'Seeking Other Accommodation', 9830, false),
+                  ('FA', 'Seeking Other Accommodation (Under 12m)', 9840, false),
+                  ('16', 'Short Home Leave', 9850, false),
+                  ('15', 'Terminal Home Leave', 9860, false),
                   ('F1', 'Training', 9870, false),
-                  ('UPW', 'Unpaid work placements', 9880, false),
-                  ('C2', 'Urgent domestic or family needs', 9890, false),
-                  ('1', 'Visit dying relative', 9900, false),
-                  ('F6', 'Visit legal adviser (unescorted)', 9910, false),
-                  ('12', 'Visit to outside hostel for assessment', 9920, false),
-                  ('F7', 'Working out scheme', 9930, false)) as t(code, description, sequence_number, active)))
+                  ('UPW', 'Unpaid Work Placements', 9880, false),
+                  ('C2', 'Urgent Domestic Or Family Needs', 9890, false),
+                  ('1', 'Visit Dying Relative', 9900, false),
+                  ('F6', 'Visit Legal Adviser (Unescorted)', 9910, false),
+                  ('12', 'Visit to Outside Hostel for Assessment', 9920, false),
+                  ('F7', 'Working Out Schemes', 9930, false)) as t(code, description, sequence_number, active)))
 
 insert
 into reference_data(domain, code, description, sequence_number, active)
@@ -245,7 +246,7 @@ with links as (select rd1.id                                                    
                where rd1.domain = 'ABSENCE_TYPE'
                  and rd1.code in ('SE')
                  and rd2.domain = 'ABSENCE_REASON'
-                 and rd2.code in ('LTX', 'C3', 'C6', 'C5', 'C7', 'C4', '4', 'SE'))
+                 and rd2.code in ('C3', 'C6', 'C5', 'C7', 'C4', '4', 'SE'))
 insert
 into reference_data_link(reference_data_id_1, reference_data_id_2, sequence_number)
 select rd1_id, rd2_id, row_num * 10
@@ -305,7 +306,7 @@ with links as (select rd1.id                                                    
                where rd1.domain = 'ABSENCE_SUB_TYPE'
                  and rd1.code in ('SPL')
                  and rd2.domain = 'ABSENCE_REASON'
-                 and rd2.code in ('LTX', 'C3', 'C6', 'C5', 'C7', 'C4', '4'))
+                 and rd2.code in ('LTX', 'C3', 'C6', 'C5', 'C7', '4'))
 insert
 into reference_data_link(reference_data_id_1, reference_data_id_2, sequence_number)
 select rd1_id, rd2_id, row_num * 10
