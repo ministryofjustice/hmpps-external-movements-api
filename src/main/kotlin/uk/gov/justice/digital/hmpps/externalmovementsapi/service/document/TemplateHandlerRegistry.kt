ae9e8904ecfb4420db462b7ed4a43b91a68f3d33
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-12-17 13:27:37 +0000

    Create TemplateHandlerRegistry

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/service/document/TemplateHandlerRegistry.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.externalmovementsapi.service.document
+
+import org.springframework.stereotype.Component
+
+@Component
+class TemplateHandlerRegistry(templateHandlers: List<DocumentTemplateHandler>) {
+  private val handlerMap = templateHandlers.associateBy { it.templateName }
+
+  fun getHandler(templateName: String): DocumentTemplateHandler =
+    handlerMap[templateName] ?: error("No handler found for template $templateName")
+
+  fun getSupportedTemplates() = handlerMap.keys
+}
