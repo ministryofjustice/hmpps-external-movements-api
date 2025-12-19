5a32dda2680717ee413a4cb0ebb438a3765b32d6
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-12-19 12:07:36 +0000

    Rename controller and remove unused endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/controller/DocumentGenerationController.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.externalmovementsapi.controller
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.externalmovementsapi.access.Roles
+import uk.gov.justice.digital.hmpps.externalmovementsapi.service.document.DocumentGenerationService
+
+@RestController
+@RequestMapping("/temporary-absence-authorisations")
+@PreAuthorize("hasAnyRole('${Roles.EXTERNAL_MOVEMENTS_UI}', '${Roles.EM_UI_TEMP}')")
+class DocumentGenerationController(private val documentGenerationService: DocumentGenerationService) {
+
+  @GetMapping("/{id}/documents")
+  fun generateDocument(@PathVariable id: String, @RequestParam format: String): ResponseEntity<ByteArray> {
+    val generatedDocument = documentGenerationService.generateDocumentFromTemplate(id, format)
+
+    val contentType = if (format.equals("pdf")) MediaType.APPLICATION_PDF else MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+    val fileExtension = if (format.equals("pdf")) "pdf" else "docx"
+
+    val headers = HttpHeaders()
+    headers.contentType = contentType
+    headers.setContentDispositionFormData("attachment", "tap-$id.$fileExtension")
+
+    return ResponseEntity.ok()
+      .headers(headers)
+      .body(generatedDocument)
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/service/document/DocumentGenerationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/service/document/DocumentGenerationService.kt
@@ -1,31 +1,18 @@
 package uk.gov.justice.digital.hmpps.externalmovementsapi.service.document
 
-import jakarta.xml.bind.JAXBElement
 import org.docx4j.Docx4J
-import org.docx4j.TraversalUtil
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage
-import org.docx4j.openpackaging.parts.WordprocessingML.BinaryPartAbstractImage
-import org.docx4j.wml.Drawing
-import org.docx4j.wml.R
-import org.docx4j.wml.Text
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import javax.imageio.ImageIO
-import kotlin.math.min
 
 @Service
 class DocumentGenerationService(private val templateHandlerRegistry: TemplateHandlerRegistry) {
 
-  fun uploadDocumentTemplate(templateName: String, document: ByteArray): String {
-    return ""
-  }
-
   fun generateDocumentFromTemplate(templateName: String, format: String): ByteArray {
     try {
       val handler = templateHandlerRegistry.getHandler(templateName)
-      val templateStream = ClassPathResource("templates/$templateName.docx").inputStream
+      val templateStream = ClassPathResource("$templateName.docx").inputStream
 
       val wordPackage = WordprocessingMLPackage.load(templateStream)
 
