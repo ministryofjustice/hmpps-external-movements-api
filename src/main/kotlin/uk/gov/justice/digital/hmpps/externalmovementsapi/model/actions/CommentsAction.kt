3924c4be70915d73bc0c3ffd7aceede5e7aa7b03
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-01-30 16:37:19 +0000

    do not override comments when truncated

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/model/actions/CommentsAction.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/model/actions/CommentsAction.kt
@@ -4,7 +4,8 @@ interface CommentsAction : Action {
   val comments: String?
   infix fun changes(comments: String?): Boolean {
     val trimmedComment = this.comments?.let {
-      it.substring(0, maxOf(it.indexOf("... see DPS"), it.length))
+      val endIndex = it.indexOf("... see DPS")
+      it.substring(0, if (endIndex > 0) endIndex else it.length)
     }
     return this.comments != comments && (trimmedComment?.let { comments?.startsWith(it)?.not() } ?: true)
   }
