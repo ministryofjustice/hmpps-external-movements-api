424053f846970d556a47e1f2b894c2368ee0563b
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-02-06 17:48:02 +0000

    Revert read auth locations to use aggregate of occurrence locations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/service/GetTapAuthorisation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/service/GetTapAuthorisation.kt
@@ -60,7 +60,7 @@ private fun TemporaryAbsenceAuthorisation.with(
   end = end,
   totalOccurrenceCount = totalOccurrences,
   occurrences = occurrences,
-  locations = locations,
+  locations = occurrences.mapTo(linkedSetOf()) { it.location },
   schedule = schedule,
   comments = comments,
 )
