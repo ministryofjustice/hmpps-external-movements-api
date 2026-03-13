cea4937c876912d7531699d01c5d2473f5f21ffa
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-03-13 09:17:29 +0000

    remove concurrently for flyway

--- src/main/resources/db/schema/V3_2__update_tap_indices.sql
+++ src/main/resources/db/schema/V3_2__update_tap_indices.sql
@@ -1,5 +1,5 @@
 drop index if exists idx_person_summary_prison_code;
-create index concurrently if not exists idx_person_summary_prison_code_identifier on person_summary (prison_code, person_identifier);
-create index concurrently if not exists idx_tap_authorisation_status_end_prison on tap.authorisation (status_id, "end", prison_code);
-create index concurrently if not exists idx_tap_occurrence_prison_start_end on tap.occurrence (prison_code, start, "end");
-create index concurrently if not exists idx_tap_occurrence_prison_end on tap.occurrence (prison_code, "end");
+create index if not exists idx_person_summary_prison_code_identifier on person_summary (prison_code, person_identifier);
+create index if not exists idx_tap_authorisation_status_end_prison on tap.authorisation (status_id, "end", prison_code);
+create index if not exists idx_tap_occurrence_prison_start_end on tap.occurrence (prison_code, start, "end");
+create index if not exists idx_tap_occurrence_prison_end on tap.occurrence (prison_code, "end");
