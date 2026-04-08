c84cdedf8d525331084a4935e155be1e8e5e6642
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-04-08 12:29:51 +0100

    MIA-1763 authorisation location changes (#336)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/model/actions/authorisation/ChangeAuthorisationLocation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/model/actions/authorisation/ChangeAuthorisationLocation.kt
@@ -4,10 +4,9 @@ import uk.gov.justice.digital.hmpps.externalmovementsapi.domain.tap.authorisatio
 import uk.gov.justice.digital.hmpps.externalmovementsapi.events.DomainEvent
 import uk.gov.justice.digital.hmpps.externalmovementsapi.events.TemporaryAbsenceAuthorisationRelocated
 import uk.gov.justice.digital.hmpps.externalmovementsapi.model.location.Location
-import java.util.SequencedSet
 
-data class ChangeAuthorisationLocations(
-  val locations: SequencedSet<Location>,
+data class ChangeAuthorisationLocation(
+  val location: Location,
   override val reason: String? = null,
 ) : AuthorisationAction {
   override fun domainEvent(taa: TemporaryAbsenceAuthorisation): DomainEvent<*> = TemporaryAbsenceAuthorisationRelocated(taa.person.identifier, taa.id)
