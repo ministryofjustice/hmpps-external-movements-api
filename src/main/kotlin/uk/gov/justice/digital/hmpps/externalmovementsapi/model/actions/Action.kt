e8b9e271567bdb333308349a79aa7be867ddafa1
Anthony Britton <anthony.britton@justice.gov.uk>
2026-04-15 14:00:01 +0100

    Fix swagger docs action reason

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/model/actions/Action.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/model/actions/Action.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.externalmovementsapi.model.actions
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 interface Action {
+  @get:Schema(nullable = true)
   val reason: String?
 }
