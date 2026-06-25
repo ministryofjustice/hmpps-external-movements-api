c8e2f8235661116c27bfde8a5f25c9c1c49d81cb
Anthony Britton <anthony.britton@justice.gov.uk>
2026-06-25 09:34:14 +0100

    MIA-1966 s3 for db restore

--- src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/domain/referencedata/ReferenceData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/externalmovementsapi/domain/referencedata/ReferenceData.kt
@@ -7,8 +7,6 @@ import jakarta.persistence.MappedSuperclass
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.externalmovementsapi.domain.cache.cacheable
-import uk.gov.justice.digital.hmpps.externalmovementsapi.tap.domain.ReferenceDataPaths
-import uk.gov.justice.digital.hmpps.externalmovementsapi.tap.domain.referencedata.absencereason.AbsenceCategorisationLinkRepository
 import java.util.UUID
 import kotlin.reflect.KClass
 
@@ -22,7 +20,6 @@ abstract class ReferenceData(
   @Id
   val id: UUID,
 ) {
-
   companion object {
     val CODE = ReferenceData::code.name
     val SEQUENCE_NUMBER = ReferenceData::sequenceNumber.name
@@ -33,31 +30,16 @@ abstract class ReferenceData(
 @Repository
 class ReferenceDataRepository(
   private val entityManager: EntityManager,
-  private val linkRepository: AbsenceCategorisationLinkRepository,
 ) {
-
   fun findAll(): List<ReferenceData> = entityManager.createQuery(
     "from uk.gov.justice.digital.hmpps.externalmovementsapi.domain.referencedata.ReferenceData",
   ).cacheable().resultList.filterIsInstance<ReferenceData>()
 
-  fun findAllByType(clazz: KClass<out ReferenceData>): List<ReferenceData> = entityManager.createQuery("from ${clazz.qualifiedName}", clazz.java).cacheable().resultList
+  fun findAllByType(clazz: KClass<out ReferenceData>): List<ReferenceData> =
+    entityManager.createQuery("from ${clazz.qualifiedName}", clazz.java).cacheable().resultList
 
   fun rdProvider(): (KClass<out ReferenceData>, String) -> ReferenceData {
     val allRd = findAll().associateBy { it::class to it.code }
     return { domain: KClass<out ReferenceData>, code: String -> requireNotNull(allRd[domain to code]) }
-  }
-
-  fun referenceDataFor(required: ReferenceDataRequired): ReferenceDataPaths = with(required) {
-    val allRd = findAll()
-    val rdLinks =
-      linkRepository.findAll().groupBy({ it.id2 to it.domain1 }, { link -> allRd.first { it.id == link.id1 } })
-    val findLinkedFrom: (UUID, ReferenceDataDomain.Code) -> List<ReferenceData> =
-      { id: UUID, domainCode: ReferenceDataDomain.Code -> rdLinks[id to domainCode] ?: emptyList() }
-    return ReferenceDataPaths(
-      allRd.filter { rd ->
-        rd::class to rd.code in requiredReferenceData().map { it.domain.clazz to it.code }
-      },
-      findLinkedFrom,
-    )
   }
 }
